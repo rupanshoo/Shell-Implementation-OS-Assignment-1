b33f185f293cb04df81ead3080d967814ee3c505
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-27 17:46:22 +0530

    header files

---
+++ Shell Implementation/Shell.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h> //for fork
+#include <fcntl.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+
+int main(){
+
+
+
+    return 0;
+}
