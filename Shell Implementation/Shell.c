ed053982c886648b947a09769b1edd7fe9843b43
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-27 18:34:26 +0530

    command prompt fn defined

--- Shell Implementation/Shell.c
+++ Shell Implementation/Shell.c
@@ -6,12 +6,26 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
+
+void command_prompt(){
+    static int first = 1;
+    if(first){   //on opening the shell for the first time, clear the screen
+        const char* CLEAR_SCREEN_ANSI = "\e[1;1H\e[2J";
+        write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+        first = 0;
+    }
+
+    printf("~~#");
+
+}
+
+
+
 int main(){
 
     while(1){
         command_prompt();   //indicator to enter your command
-
-
+        exit(0);
     }
 
 
