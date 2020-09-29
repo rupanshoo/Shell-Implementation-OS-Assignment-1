b0ba8b0aba0fd83ee56dc663b279a6eab6a5b50f
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-30 01:31:32 +0530

    testing

--- Shell Implementation/rm.c
+++ Shell Implementation/rm.c
@@ -3,5 +3,22 @@
 #include <string.h>
 
 int main(int argc, char *argv[]){
-    printf("in rm");
+    char prompt;
+    if(strcmp(argv[1],"-i")==0){
+        printf("Are you sure that you want to remove %s?\n(Enter Y --> yes & N --> no\n)", argv[2]);
+        int ans = fgetc(stdin);
+        prompt = (char) ans;
+        if(prompt == 'Y'){
+            remove(argv[2]);
+        }
+        else{
+            printf("File not removed!\n");
+            return 0;
+        }
+    }
+
+    remove(argv[1]);
+    printf("File removed successfully!");
+    
+    return 0;
 }
