0a280dd1cb2825cca55c2c09093097b0df5d8d41
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-29 20:26:24 +0530

    ls basic and -a implemnted

--- Shell Implementation/ls.c
+++ Shell Implementation/ls.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/types.h>  //for opendir()
+#include <string.h>
 
-int main(int argc, char *argv){
-    printf("We are in ls!!");
+
+
+int main(int argc, char *argv[]){
+    DIR *dir;   //make a directory
+    struct dirent *content;
+    struct stat s;   //help in getting size of each file
+
+    char buffer[700];  //to help get size of each file too 
+
+    dir = opendir(".");  //to open current directory
+    if(dir == NULL){
+        printf("ERROR! Can't open directory\n");
+        return 0;
+    }
+
+    while((content = readdir(dir)) != NULL){
+        if(strcmp(argv[1],"-a")==0){
+            printf(">> %s\n", content->d_name);
+        }
+        else{
+            if(content->d_name[0] == '.') continue;
+            printf(">> %s\n", content->d_name);
+        }
+        
+    }
+
+    closedir(dir);
+
+
     return 0;
 }
