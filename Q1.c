a0816442a1b2f86f4771740465674080a26ef7b2
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-30 23:12:45 +0530

    Q1 fixed

--- Q1.c
+++ Q1.c
@@ -6,52 +6,21 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-
 int main(){
+
     int ret;
     ret = fork();
     int fd = open("csv-os.csv", O_RDONLY);   //opening the file in read-only mode
-
     int stat;
+
+
+
 
     if(fd != -1){
 
-        if(ret > 0){    //parent process
-            waitpid(ret, &stat, 0);
-            if (WIFSIGNALED(stat)){
-                printf("Error\n");
-            }
-            else if (WEXITSTATUS(stat)) {
-                printf("Exited Normally\n");
-            }
-            printf("Parent: %d\n", getpid());
-
-            char entry[6800];
-            char delim[] = ",\n\r";
-            read(fd, entry, sizeof(entry));
-            char* ptr;
-            int chA = 'B';
-
-            ptr = strchr(entry, chA);
-            //printf("%s\n", entry);
-            //printf("%c \n", (char)entry[33]);
-            char *token = strtok(entry, delim);
-            char arr[400][6][3];
-            int i=0;
-            while(token!=NULL){
-                //printf("%s\n", token);
-                strcpy(arr[i/6][i%6],token);
-                token = strtok(NULL, delim);
-                i++;
-            }
-            for(int i=0;i<400;i++){
-                printf("%s - %s - %s - %s - %s -Avg: " , arr[i][0], arr[i][2], arr[i][3], arr[i][4], arr[i][5]);
-                int sum=atoi(arr[i][2])+atoi(arr[i][3])+atoi(arr[i][4])+atoi(arr[i][5]);
-                printf("%d\n",sum/4);
-            }
-        }    //parent process
-
         if(ret == 0){  //child proces
+            printf("CHILD: %d\n", getpid());
+
             char entry[6800];
             char delim[] = ",\n\r";
             read(fd, entry, sizeof(entry));
@@ -72,21 +41,56 @@ int main(){
             }
             for(int i=0;i<400;i++){
                 int sum=atoi(arr[i][2])+atoi(arr[i][3])+atoi(arr[i][4])+atoi(arr[i][5]);
-                int average = sum/4;
-                printf("%s - %s - %s - %s - %s -Avg: " , arr[i][0], arr[i][2], arr[i][3], arr[i][4], arr[i][5]);
-                //write(STDOUT_FILENO, );
-                write(STDOUT_FILENO, &average, sizeof(average));
+                if(strcmp("A",arr[i][1])==0) {
+                    printf("%s = %d\n", arr[i][1], sum / 4);
+                }
             }
             sleep(2);
             exit(EXIT_SUCCESS);
         }
-    
+
+        waitpid(ret, &stat, 0);
+        if (WIFSIGNALED(stat)){
+            printf("Error\n");
+        }
+        else if (WEXITSTATUS(stat)) {
+            printf("Exited Normally\n");
+        }
+        printf("Parent: %d\n", getpid());
+
+        char entry[6800];
+        char delim[] = ",\n\r";
+        read(fd, entry, sizeof(entry));
+        char* ptr;
+        int chA = 'B';
+
+        ptr = strchr(entry, chA);
+        //printf("%s\n", entry);
+        //printf("%c \n", (char)entry[33]);
+        char *token = strtok(entry, delim);
+        char arr[400][6][3];
+        int i=0;
+        while(token!=NULL){
+            //printf("%s\n", token);
+            strcpy(arr[i/6][i%6],token);
+            token = strtok(NULL, delim);
+            i++;
+        }
+        for(int i=0;i<400;i++){
+            int sum=atoi(arr[i][2])+atoi(arr[i][3])+atoi(arr[i][4])+atoi(arr[i][5]);
+            if(strcmp("B",arr[i][1])==0) {
+                printf("%s = %d\n", arr[i][1], sum / 4);
+            }
+        }
+
+
     }
 
     else{
         printf("Error!! Unable to open the provided file");
         exit(1);
     }
+
 
     close(fd);
 
