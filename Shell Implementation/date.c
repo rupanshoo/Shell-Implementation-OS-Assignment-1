b4b63439923d339734cd45c4c5b069c3fc228fc0
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-30 08:28:06 +0530

    date -u and basic done

--- Shell Implementation/date.c
+++ Shell Implementation/date.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
     time_t t;  //define time variable
     time(&t);
-    printf("Current date and time: %s", ctime(&t));
+
+    struct tm *uniTime;
+
+    if(strcmp(argv[1], "-u")==0){
+        uniTime = gmtime(&t);
+        printf("Current UTC Time (hr:min:sec) - 24 hour clock: %d:%d:%d" , uniTime->tm_hour, uniTime->tm_min, uniTime->tm_sec);
+    }
+    else{
+        printf("Current date and time: %s\n", ctime(&t));
+    }
+    
     return 0;
 }
