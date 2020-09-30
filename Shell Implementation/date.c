98b90dde7e55fff1e8992222c012d2eba00901e7
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-30 08:50:57 +0530

    date -u and -s done

--- Shell Implementation/date.c
+++ Shell Implementation/date.c
@@ -8,11 +8,18 @@ int main(int argc, char* argv[]){
     time(&t);
 
     struct tm *uniTime;
+    struct tm tim;
 
     if(strcmp(argv[1], "-u")==0){
         uniTime = gmtime(&t);
         printf("Current UTC Time (hr:min:sec) - 24 hour clock: %d:%d:%d" , uniTime->tm_hour, uniTime->tm_min, uniTime->tm_sec);
     }
+
+    else if(strcmp(argv[1], "-s")==0){
+        printf("Can't set date as not permitted by System\n");
+        printf("Date Entered in format [dd mm yyyy] : %s/%s/%s\n", argv[2], argv[3], argv[4]);
+    }
+
     else{
         printf("Current date and time: %s\n", ctime(&t));
     }
