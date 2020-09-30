304b8811ce7842e3888b7c770a80ac43db576ac7
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-09-30 06:47:10 +0530

    mkdir basic implemented

--- Shell Implementation/mkdir.c
+++ Shell Implementation/mkdir.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 int main(int argc, char *argv[]){
-    printf("MKDIR");
+    int ch;  //to check if executed properly or not
+    ch = mkdir(argv[1],0777);
+    if(ch == 0){
+        printf("Successfully created directory!\n");
+    }
+    if(ch == -1){
+        printf("Unsuccessful! Directory not created.\n");
+    }
     return 0;
 }
